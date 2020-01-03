b570fb4f40793cecfec3a9bed4fea67662f583de
tanliansheng <tanliansheng2011@gmail.com>
2020-01-03 10:08:13 +0800

    优雅停机 四改

--- dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/ProtocolConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/ProtocolConfig.java
@@ -31,6 +31,7 @@ import com.alibaba.dubbo.remoting.Transporter;
 import com.alibaba.dubbo.remoting.exchange.Exchanger;
 import com.alibaba.dubbo.remoting.telnet.TelnetHandler;
 import com.alibaba.dubbo.rpc.Protocol;
+import com.alibaba.dubbo.rpc.protocol.AbstractProtocol;
 
 /**
  * ProtocolConfig
@@ -474,8 +475,7 @@ public class ProtocolConfig extends AbstractConfig {
         //这一句是新版dubbo的关键改动之处。老版本没有这几行sleep的代码【请读者自行阅读老版本的源码】。默认10秒，可以通过 [dubbo.service.shutdown.wait] 配置
         //
         try {
-//            Thread.sleep(AbstractProtocol.getServerShutdownTimeout());
-            Thread.sleep(Constants.DEFAULT_SERVER_SHUTDOWN_TIMEOUT);
+            Thread.sleep(AbstractProtocol.getServerShutdownTimeout());
         } catch (InterruptedException e) {
             logger.warn("Interrupted unexpectedly when waiting for registry notification during shutdown process!");
         }
